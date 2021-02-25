fa095c84d462fe12463fb512dac9e667da58c2ec
Braiden <bmxfiend1@gmail.com>
2021-02-25 18:39:12 -0500

    Update web_scraper.py

--- WebScrapers/WebScraperOne/web_scraper.py
+++ WebScrapers/WebScraperOne/web_scraper.py
@@ -3,9 +3,6 @@
 
     We are scraping all of the commands and descriptions off of a tutorials points
     website.
-    
-    The result of all of this will be pushed to a file, this file will be made
-    in the local directory if it does not exist !
 
     This my first webscraper !
 """
@@ -32,11 +29,11 @@ class WebScraper:
         soup = BeautifulSoup(webpage.content, "html.parser")
         commands = soup.find_all("td")
         with open("output.txt", 'w') as outputFile:
-            for os in commands:
-                outputFile.writelines(os.text + "\n\n")
+            for webEle in commands:
+                outputFile.writelines(webEle.text + "\n\n")
 
 if __name__ == "__main__":
-    
+
     _url = "https://www.tutorialspoint.com/batch_script/batch_script_commands.htm"
     scrape = WebScraper(_url)
     scrape.scrape_commands()
